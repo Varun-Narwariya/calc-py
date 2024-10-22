d44734014aa595a07f691a24c2935b90ffce8b64
Varun-Narwariya <varunlodhi2233@gmail.com>
2024-10-22 18:39:17 +0530

    calc

---
+++ calc.py
@@ -0,0 +1,11 @@
+a=int(input("enter a no :"))
+b=int(input("enter a no :"))
+o=input("enter operation")
+if(o=="+"):
+    print (a+b)
+elif(o=="-"):
+    print (a-b)
+elif(o=="*"):
+    print (a*b)
+elif(o=="/"):
+    print (a/b)
